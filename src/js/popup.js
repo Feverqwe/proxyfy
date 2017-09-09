68598e38b191fc556c5a63daa80a4893db23b66e
Anton <anton9121@gmail.com>
2017-09-09 12:55:33 +0500

    format

--- src/js/popup.js
+++ src/js/popup.js
@@ -5,90 +5,93 @@ const utils = require('./utils');
 const tmpl = require('./tmpl');
 
 utils.getSyncStorage({
-  proxyList: []
+    proxyList: []
 }).then(function (storage) {
-  const proxyList = storage.proxyList;
-  const switcherNode = document.querySelector('.switcher');
+    const proxyList = storage.proxyList;
+    const switcherNode = document.querySelector('.switcher');
 
-  const typeMenuItems = {};
 
-  const onSelect = (function () {
-    let lastSelectedItem = null;
-    return function () {
-      if (lastSelectedItem) {
-        lastSelectedItem.node.classList.remove('item__selected');
-      }
-      this.node.classList.add('item__selected');
-      lastSelectedItem = this;
-    };
-  })();
+    return;
 
-  const menuItems = proxyList.concat([
-    {name: 'Disable', type: 'disable'},
-    {name: 'Options', type: 'options'}
-  ]).map(function (proxyObj, index) {
-    const node = tmpl.createElement('a', {
-      class: 'item',
-      href: '#',
-      data: {
-        index: index
-      },
-      append: [
-        tmpl.createElement('div', {
-          class: 'status'
-        }),
-        tmpl.createElement('div', {
-          class: 'name'
-        }, proxyObj.name)
-      ]
+    const typeMenuItems = {};
+
+    const onSelect = (function () {
+        let lastSelectedItem = null;
+        return function () {
+            if (lastSelectedItem) {
+                lastSelectedItem.node.classList.remove('item__selected');
+            }
+            this.node.classList.add('item__selected');
+            lastSelectedItem = this;
+        };
+    })();
+
+    const menuItems = proxyList.concat([
+        {name: 'Disable', type: 'disable'},
+        {name: 'Options', type: 'options'}
+    ]).map(function (proxyObj, index) {
+        const node = tmpl.createElement('a', {
+            class: 'item',
+            href: '#',
+            data: {
+                index: index
+            },
+            append: [
+                tmpl.createElement('div', {
+                    class: 'status'
+                }),
+                tmpl.createElement('div', {
+                    class: 'name'
+                }, proxyObj.name)
+            ]
+        });
+
+        const itemObj = {
+            node: node,
+            proxyObj: proxyObj,
+            select: onSelect
+        };
+
+        if (proxyObj.type) {
+            typeMenuItems[proxyObj.type] = itemObj;
+        }
+
+        switcherNode.appendChild(node);
+
+        return itemObj;
     });
 
-    const itemObj = {
-      node: node,
-      proxyObj: proxyObj,
-      select: onSelect
-    };
+    switcherNode.addEventListener('click', function (e) {
+        e.preventDefault();
+        const node = utils.closestNode(this, e.target);
+        if (node) {
+            const menuItem = menuItems[node.dataset.index];
 
-    if (proxyObj.type) {
-      typeMenuItems[proxyObj.type] = itemObj;
-    }
+            if (menuItem.proxyObj.type === 'options') {
+                chrome.tabs.create({url: 'options.html'});
+            } else {
+                if (menuItem.proxyObj.type === 'disable') {
+                    chrome.runtime.sendMessage({action: 'clearProxySettings'});
+                } else {
+                    chrome.runtime.sendMessage({action: 'setProxySettings', proxyObj: menuItem.proxyObj});
+                }
+                menuItem.select();
+            }
+        }
+    });
 
-    switcherNode.appendChild(node);
-
-    return itemObj;
-  });
-
-  switcherNode.addEventListener('click', function (e) {
-    e.preventDefault();
-    const node = utils.closestNode(this, e.target);
-    if (node) {
-      const menuItem = menuItems[node.dataset.index];
-
-      if (menuItem.proxyObj.type === 'options') {
-        chrome.tabs.create({url: 'options.html'});
-      } else {
-        if (menuItem.proxyObj.type === 'disable') {
-          chrome.runtime.sendMessage({action: 'clearProxySettings'});
-        } else {
-          chrome.runtime.sendMessage({action: 'setProxySettings', proxyObj: menuItem.proxyObj});
+    utils.sendMessage({action: 'getProxySettings'}).then(function (response) {
+        const proxyObj = response.proxyObj;
+        let menuItem = null;
+        proxyObj && menuItems.some(function (item) {
+            if (item.proxyObj.name === proxyObj.name) {
+                menuItem = item;
+                return true;
+            }
+        });
+        if (!menuItem) {
+            menuItem = typeMenuItems.disable;
         }
         menuItem.select();
-      }
-    }
-  });
-
-  utils.sendMessage({action: 'getProxySettings'}).then(function (response) {
-    const proxyObj = response.proxyObj;
-    let menuItem = null;
-    proxyObj && menuItems.some(function (item) {
-      if (item.proxyObj.name === proxyObj.name) {
-        menuItem = item;
-        return true;
-      }
     });
-    if (!menuItem) {
-      menuItem = typeMenuItems.disable;
-    }
-    menuItem.select();
-  });
 });
--- src/js/utils.js
+++ src/js/utils.js
@@ -1,34 +1,34 @@
 const utils = {};
 
 utils.closestNode = function (parent, someChild) {
-  if (parent === someChild) {
-    return null;
-  }
-  if (!parent.contains(someChild)) {
-    return null;
-  }
-  let parentNode;
-  while (parentNode = someChild.parentNode) {
-    if (parentNode !== parent) {
-      someChild = parentNode;
-    } else {
-      return someChild;
+    if (parent === someChild) {
+        return null;
     }
-  }
+    if (!parent.contains(someChild)) {
+        return null;
+    }
+    let parentNode;
+    while (parentNode = someChild.parentNode) {
+        if (parentNode !== parent) {
+            someChild = parentNode;
+        } else {
+            return someChild;
+        }
+    }
 };
 
 utils.sendMessage = function () {
-  const args = [].slice.call(arguments);
-  return new Promise(function (resolve) {
-    args.push(resolve);
-    chrome.runtime.sendMessage.apply(chrome.runtime, args);
-  });
+    const args = [].slice.call(arguments);
+    return new Promise(function (resolve) {
+        args.push(resolve);
+        chrome.runtime.sendMessage.apply(chrome.runtime, args);
+    });
 };
 
 utils.getSyncStorage = function (data) {
-  return new Promise(function (resolve) {
-    chrome.storage.sync.get(data, resolve);
-  });
+    return new Promise(function (resolve) {
+        chrome.storage.sync.get(data, resolve);
+    });
 };
 
 module.exports = utils;
