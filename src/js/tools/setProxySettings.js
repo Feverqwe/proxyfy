a5193f08e0210abe41ee1152999fc87b3bd430bd
Anton <Feverqwe@users.noreply.github.com>
2018-04-22 06:50:43 +0500

    1

--- src/js/tools/setProxySettings.js
+++ src/js/tools/setProxySettings.js
@@ -2,28 +2,38 @@ import promisifyApi from "./promisifyApi";
 
 const setProxySettings = profile => {
   const meta = '//' + JSON.stringify({proxyfy: profile.name});
+  const regexpRules = [];
+  profile.rules.forEach(rule => rule.getPatterns().forEach(pattern => {
+    if (pattern.type === 'regexp') {
+      regexpRules.push(pattern.pattern);
+    }
+  }));
   const config = {
     mode: 'pac_script',
     pacScript: {
-      data: `${meta}\nvar FindProxyForURL=${function (rulesStrRe, invertRules, proxyUrl) {
+      data: `${meta}\nvar FindProxyForURL=${function (rulesStrRe, invertRules, scheme, proxyUrl) {
         const re = rulesStrRe && new RegExp(rulesStrRe);
         return function (url) {
           let r = true;
           if (re) {
-            r = re.test(url);
+            const m = /^([^:]+:\/\/[^\/?#]+)/.exec(url);
+            if (m) {
+              r = re.test(m[1]); 
+            }
             if (invertRules) {
               r = !r;
             }
           }
           if (r) {
-            return "PROXY " + proxyUrl;
+            return scheme + ' ' + proxyUrl;
           } else {
             return "DIRECT";
           }
         };
       }})(${[
-        profile.rules.map(rule => rule.getPattern()).join('|'),
+        regexpRules.join('|'),
         profile.invertRules,
+        profile.proxy.getScheme().toUpperCase(),
         profile.proxy.getUrl()
       ].map(JSON.stringify).join(',')});`
     }
