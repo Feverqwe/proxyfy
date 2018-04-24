e3aba147e9cd9e44cd2bc5a2bf6f2edc09dd61cd
Anton <anton9121@gmail.com>
2018-04-24 10:28:45 +0500

    rm cache

--- src/js/pacScript.js
+++ src/js/pacScript.js
@@ -1,46 +1,40 @@
 FindProxyForURL = (function () {
   const {regexpPatterns, cidrPatterns, invertBypassList, proxies} = config;
   const URL = require('url-parse');
-  const Cache = require('./tools/cache').default;
   const getIpAddrFromHostname = require('./tools/getIpAddrFromHostname').default;
   const ip6addr = require('ip6addr');
 
-  const cache = new Cache(1000);
   const originRe = regexpPatterns.length !== 0 && new RegExp(regexpPatterns.join('|'));
   const cidrList = cidrPatterns.length !== 0 && cidrPatterns.map(pattern => ip6addr.createCIDR(pattern));
 
   return function (url) {
     const {protocol, origin, hostname} = new URL(url);
 
-    let proxy = cache.get(origin);
-    if (proxy === undefined) {
-      let inBypassList = originRe && originRe.test(origin);
-      if (!inBypassList && cidrList) {
-        const ipAddr = getIpAddrFromHostname(hostname);
-        if (ipAddr) {
-          inBypassList = cidrList.some(cidr => cidr.contains(ipAddr));
-        }
+    let inBypassList = originRe && originRe.test(origin);
+    if (!inBypassList && cidrList) {
+      const ipAddr = getIpAddrFromHostname(hostname);
+      if (ipAddr) {
+        inBypassList = cidrList.some(cidr => cidr.contains(ipAddr));
       }
+    }
 
-      let useProxy = true;
-      if (inBypassList) {
-        useProxy = false;
-      }
-      if (invertBypassList) {
-        useProxy = !useProxy;
-      }
+    let useProxy = true;
+    if (inBypassList) {
+      useProxy = false;
+    }
+    if (invertBypassList) {
+      useProxy = !useProxy;
+    }
 
-      if (useProxy) {
-        proxy = proxies.singleProxy;
+    let proxy = null;
+    if (useProxy) {
+      proxy = proxies.singleProxy;
+      if (!proxy) {
+        proxy = proxies[protocol];
         if (!proxy) {
-          proxy = proxies[protocol];
-          if (!proxy) {
-            proxy = proxies.fallbackProxy;
-          }
+          proxy = proxies.fallbackProxy;
         }
       }
-
-      cache.set(origin, proxy || null);
     }
 
     if (proxy) {
