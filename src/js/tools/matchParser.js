83db9353080190353daa1e67031004c96b0e0ab5
Anton <anton9121@gmail.com>
2018-04-24 15:06:34 +0500

    1

--- src/js/tools/matchParser.js
+++ src/js/tools/matchParser.js
@@ -1,6 +1,10 @@
 const matchParser = pattern => {
   const patterns = [];
 
+  if (/^\./.test(pattern)) {
+    pattern = '*' + pattern;
+  }
+
   patterns.push(pattern);
   if (/^\*\./.test(pattern.substr(2))) {
     patterns.push(pattern);
