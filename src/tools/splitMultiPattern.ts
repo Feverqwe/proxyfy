fe32f4a34f522a2f4fde1a911bacb87316304d5d
Anton <leonardspbox@gmail.com>
2022-07-12 09:36:57 +0500

    fix

--- src/tools/splitMultiPattern.ts
+++ src/tools/splitMultiPattern.ts
@@ -1,5 +1,8 @@
 function splitMultiPattern(pattern: string) {
-  return pattern.split(/[,\n]/).map(v => v.trim()).filter(v => !/^#/.test(v) && v.length);
+  return pattern.split(/\n/).map(s => s.trim()).filter(s => !/^#/.test(s)).reduce<string[]>((acc, line) => {
+    acc.push(...line.split(/,/).map(s => s.trim()).filter(s => s.length));
+    return acc;
+  }, []);
 }
 
 export default splitMultiPattern;
